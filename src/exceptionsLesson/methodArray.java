03bbf307a9e06159eb12c091650b7679a87d9f3c
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-01-18 19:48:03 +0400

    Что-то пытался сделать, ничего не понятно

--- src/exceptionsLesson/methodArray.java
+++ src/exceptionsLesson/methodArray.java
@@ -5,7 +5,7 @@ import exceptionsLesson.exceptions.MyArraySizeException;
 public class methodArray {
      String[][] methodArray = new String[][]{{"1", "2", "3", "4"}, {"4", "3", "2","1"}, {"1", "2", "2"}, {"2", "1", "1", "ABC"}};
      try {
-        System.out.println(methodArray);
+        System.out.println(methodArray.length);
     } catch (MyArraySizeException e) {
         System.out.println("Размер должен соответствовать 4*4!");
     }
