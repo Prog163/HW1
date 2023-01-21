9d8eee0c7d53f7bce9003900bb2c45b6b008c016
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-01-21 14:18:39 +0400

    Сделано преобразование в int, подсчёт суммы и добавлен MyArrayDataException

--- src/exceptionsLesson/ArrayWorker.java
+++ src/exceptionsLesson/ArrayWorker.java
@@ -1,5 +1,25 @@
 package exceptionsLesson;
 
+import exceptionsLesson.exceptions.MyArrayDataException;
+
 public class ArrayWorker {
-    private String[][] arrayOfStrings = new String[][]{{"1", "2", "3", "4"}, {"4", "3", "2", "1"}, {"1", "2", "2"}, {"2", "1", "1", "ABC"}};
+    private String[][] arrayOfStrings = new String[][]{{"1", "2", "3", "4"}, {"A", "3", "2", "1"}, {"1", "2", "2", "2"}, {"2", "1", "1", "2"}};
+
+    public void elementsOfArray() throws MyArrayDataException {
+
+        int result = 0;
+
+        for (int i = 0; i < arrayOfStrings.length; i++) {
+            for (int j = 0; j < arrayOfStrings[i].length; j++) {
+                String currentString = arrayOfStrings[i][j];
+                try {
+                    Integer currentInt = Integer.parseInt(currentString);
+                    result = result + currentInt;
+                } catch (NumberFormatException e) {
+                  throw new MyArrayDataException("Неправильный символ в ячейке массива " + i + " " + j);
+                }
+            }
+        }
+        System.out.println(result);
+    }
 }
--- src/exceptionsLesson/Main.java
+++ src/exceptionsLesson/Main.java
@@ -1,8 +1,15 @@
 package exceptionsLesson;
 
+import exceptionsLesson.exceptions.MyArrayDataException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        ArrayWorker arrayWorker = new ArrayWorker();
+        try {
+            arrayWorker.elementsOfArray();
+        } catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
     }
 }
