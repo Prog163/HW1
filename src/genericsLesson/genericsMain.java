9a04a276a78f91b124b4381739c7fca4f66976e1
Aleksandr <Aleksandr.zaichikov@simbirsoft.com>
2023-02-16 18:41:31 +0400

    Добавил вариант через метод Collections.swap

--- src/genericsLesson/genericsMain.java
+++ src/genericsLesson/genericsMain.java
@@ -1,6 +1,8 @@
 package genericsLesson;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class genericsMain {
     public static void main (String[] args) {
@@ -12,6 +14,16 @@ public class genericsMain {
         array[1] = a;
         System.out.println(Arrays.toString(array));
 
+        //Ещё нарыл такой способ решения, через метод Collections.swap
+        ArrayList <String> arrList = new ArrayList<String>();
+        arrList.add("One");
+        arrList.add("Two");
+        arrList.add("Three");
+        arrList.add("Four");
+        System.out.println(arrList);
+
+        Collections.swap(arrList, 1, 2);
+        System.out.println(arrList);
     }
 
 
